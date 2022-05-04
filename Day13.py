d6ee481ae9ca3f34c55b5b58899a17d8f1769cd9
steinerML <102678863+steinerML@users.noreply.github.com>
2022-05-04 13:05:26 +0200

    Add files via upload

---
+++ Day13.py
@@ -0,0 +1,21 @@
+#Learning how to add strings to printed results.
+
+vervoermiddel = "auto"
+
+vervoermiddel2 = vervoermiddel + "mobiel"
+
+print(vervoermiddel)
+print (vervoermiddel2)
+
+
+
+telefoonnummer = "0987654321"
+
+print ("Mijn telefoonnummer is", telefoonnummer)
+print ("Mijn telefonnummer is " + telefoonnummer)
+
+
+leeftijd = 46
+
+print ("Mijn leeftijd is", leeftijd)
+print ("Mijn leeftijd is " + str(leeftijd))
